93949e8d152804ccdc104483c7ca4977ed2186c7
sabrinaroxana <sr.sabrina1@gmail.com>
2022-11-15 19:49:51 -0300

    commit projeto

---
+++ projetointroweb.js
@@ -0,0 +1,139 @@
+const turmas = [
+    {
+        turma: "Hipátia",
+        curso: "JavaScript",
+        inicio: "30/11/2022",
+        termino: "30/01/2023",
+        numeroAluno: 150,
+        periodo: "Noturno",
+        concluido: false
+    },
+    {
+        turma: "Sibyla",
+        curso: "JavaScript",
+        inicio: "30/10/2022",
+        termino: "30/12/2022",
+        numeroAluno: 200,
+        periodo: "Integral",
+        concluido: false
+    },
+    {
+        turma: "Curie",
+        curso: "HTML e CSS",
+        inicio: "15/09/2022",
+        termino: "15/10/2022",
+        numeroAluno: 180,
+        periodo: "Noturno",
+        concluido: true
+    },
+    {
+        turma: "Zhenyi",
+        curso: "HTML e CSS",
+        inicio: "01/11/2022",
+        termino: "01/01/2023",
+        numeroAluno: 80,
+        periodo: "Integral",
+        concluido: false
+    },
+    {
+        turma: "Clarke",
+        curso: "HTML e CSS",
+        inicio: "04/07/2022",
+        termino: "04/09/2022",
+        numeroAluno: 200,
+        periodo: "Noturno",
+        concluido: true
+    },
+    {
+        turma: "Blackwell",
+        curso: "APIsRest",
+        inicio: "20/03/2022",
+        termino: "20/06/2022",
+        numeroAluno: 100,
+        periodo: "Integral",
+        concluido: true
+    },
+    {
+        turma: "Elion",
+        curso: "APIsRest",
+        inicio: "12/01/2022",
+        termino: "12/06/2022",
+        numeroAluno: 200,
+        periodo: "Noturno",
+        concluido: true
+    },
+    {
+        turma: "Burnell",
+        curso: "APIsRest",
+        inicio: "18/10/2022",
+        termino: "18/04/2023",
+        numeroAluno: 90,
+        periodo: "Integral",
+        concluido: false
+    },
+];
+
+const cursos = [
+    {
+        curso: "HTML e CSS",
+        descricao: " Aprenda HTML e CSS em apenas 1 mês!",
+        duracao: "1 Mês",
+        valor: 500
+    },
+    {
+        curso: "JavaScript",
+        descricao: "Aprenda de uma vez por todas JavaScript e se destaque no mercado de trabalho!",
+        duracao: "2 Meses",
+        valor: 900
+    },
+    {
+        curso: "APIsRest",
+        descricao: "Aprenda com a melhor instituição de ensino tudo sobre APIsRest!",
+        duracao: "6 Meses",
+        valor: 2000
+    }
+];
+
+const estudantes = [
+    {
+        estudante: "Chris Evans",
+        turma: "Hipátia",
+        curso: "JavaScript",
+        valor: "900,00",
+        nParcelas: 9,
+        desconto: false,
+        parcelas: 100
+    },
+    {
+        estudante: "Halle Berry",
+        turma: "Burnell",
+        curso: "APIsRest",
+        valor: "2.000,00",
+        nParcelas: 4,
+        desconto: false,
+        parcelas: 500
+    },
+    {
+        estudante: "Lashana Lynch",
+        turma: "Zhenyi",
+        curso: "HTML e CSS",
+        valor: "",
+        nParcelas: ,
+        desconto: true
+        parcelas: 
+    }
+];
+
+const parcelarCurso = (parcelas) => {
+    if(parcelas <= 2){
+        cursos[1].valor === parcelas - 20%
+               
+    }else if{
+
+        console.log("O valor da sua ")
+    } 
+}
+
+ 
+
+kkkkk
